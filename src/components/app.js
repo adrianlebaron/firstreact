66dabca4a72b594d71fbbae767116a8b5f065b6c
A.D <adrianlebaron@gmail.com>
2018-11-26 12:00:16 -0700

    button is broken

--- src/components/app.js
+++ src/components/app.js
@@ -1,11 +1,16 @@
 import React, { Component } from 'react';
 import Header from './header';
+import {Button} from './button'
 
 export default class App extends Component {
   render() {
     return (
       <div className='app'>
-          <Header />   
+          <Header />  
+          <Header /> 
+          < Button title="you are wasting your time pressing my buttons" go away="now" />
+          < Button title="if you click on me I'll do nothing" body="try me"/>
+          < Button title="you can't make me do anything" />
       </div>
     );
   }
